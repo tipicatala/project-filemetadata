d85b89005c8423e562f66b16914f8da989643a8c
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2021-10-11 13:48:27 +0300

    added file info

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const bodyParser = require("body-parser");
+const shortId = require("shortid");
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size,
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
